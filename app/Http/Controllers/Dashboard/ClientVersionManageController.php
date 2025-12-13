5a1964cc65e82f4baa986dcc3a420eb35b31706f
Pooya Behravesh <pooyabehravesh@gmail.com>
2025-12-13 21:15:53 +0330

    Upload new Client Application file controller method completed

--- app/Http/Controllers/Dashboard/ClientVersionManageController.php
+++ app/Http/Controllers/Dashboard/ClientVersionManageController.php
@@ -8,7 +8,28 @@ use Illuminate\Http\Request;
 class ClientVersionManageController extends Controller
 {
     // Show upload file form
-    public function show() {
+    public function show()
+    {
         return view('dashboard.versionmanage.upload-file');
+    }
+
+
+    // Upload new Client Version
+    public function upload(Request $request)
+    {
+        $request->validate([
+            'version' => 'required|string',
+            'apk' => 'required|file|mimes:apk,zip',
+        ]);
+
+        $version = $request->input('version');
+        $fileName = 'nrp-' . $version . '.apk';
+
+        $request->file('apk')->storeAs(
+            'public/apks',
+            $fileName
+        );
+
+        return back()->with('success', 'APK uploaded successfully');
     }
 }
